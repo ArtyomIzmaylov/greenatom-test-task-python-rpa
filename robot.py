43c86b3f9b158b8aa5e71e0925fe71f82e98239e
ArtyomIzmaylov <artyomguybov2002@gmail.com>
2024-04-02 20:55:40 +0300

    Revert "добавил start_number query параметр в ручку"
    This reverts commit 75d10d0b49ea8ca84e3f02ef3d861c5c17a0b6d5.

--- robot.py
+++ robot.py
@@ -1,8 +1,8 @@
 import asyncio
 import sys
 
-async def robot(start_number=0):
-    counter = start_number
+async def robot():
+    counter = 0
     while True:
         print(counter)
         counter += 1
@@ -10,5 +10,4 @@ async def robot(start_number=0):
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "run":
-        start_number = int(sys.argv[2]) if len(sys.argv) > 2 else 0
-        asyncio.run(robot(start_number))
+        asyncio.run(robot())
